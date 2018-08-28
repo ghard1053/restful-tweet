464cbee81753f3873342eabab8fb1421c2bb0087
Takao <sukisya70@gmail.com>
2018-08-28 19:32:58 +0900

    User Tweet Class

---
+++ src/main/java/com/pad/rest/tweet/restfultweet/user/User.java
@@ -0,0 +1,108 @@
+package com.pad.rest.tweet.restfultweet.user;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+//import io.swagger.annotations.ApiModel;
+//import io.swagger.annotations.ApiModelProperty;
+
+//@ApiModel(description="All details about the user.")
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@Size(min=3, message="username should hove atleast 3 characters")
+	//@ApiModelProperty(notes="Name should have atleast 2 characters")
+	private String username;
+
+	@Size(min=3, message="Name should hove atleast 3 characters")
+	//@ApiModelProperty(notes="Name should have atleast 2 characters")
+	private String name;
+	
+	@Size(min=2, message="Name should hove atleast 2 characters")
+	//@ApiModelProperty(notes="Name should have atleast 2 characters")
+	private String mail;
+	
+	private String bio;	
+
+	@OneToMany(mappedBy="user")
+	private List<Tweet> tweets;
+
+	
+	protected User() {
+		
+	}
+	
+	public User(Integer id, String username, String name, String mail, String bio) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.name = name;
+		this.mail = mail;
+		this.bio = bio;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+	public List<Tweet> getTweets() {
+		return tweets;
+	}
+
+	public void setTweets(List<Tweet> tweets) {
+		this.tweets = tweets;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("User [id=%s, username=%s, name=%s, mail=%s, bio=%s]", id, username, name, mail, bio);
+	}
+	
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user values(10001, 'taka33', 'taka', 'taka@sample.com', 'hello!');
+insert into user values(10002, 'kiki33', 'kiki', 'kiki@sample.com', 'hello!');
+insert into user values(10003, 'aya33', 'aya', 'aya@sample.com', 'hello!');
+insert into tweet values(11001, 'my first tweet', 10001);
+insert into tweet values(11002, 'my second tweet', 10001);

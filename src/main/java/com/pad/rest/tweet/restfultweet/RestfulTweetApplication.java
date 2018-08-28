b589b266e7a971b1262008bbeb9fdabec333b6df
Takao <>
2018-08-28 18:39:44 +0900

    init

---
+++ src/main/java/com/pad/rest/tweet/restfultweet/RestfulTweetApplication.java
@@ -0,0 +1,12 @@
+package com.pad.rest.tweet.restfultweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulTweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulTweetApplication.class, args);
+	}
+}
